c342d93fad346612f98a7fd9ae6d5f292b388359
Jose Sicajan <jlsicajan@gmail.com>
2020-05-27 04:45:31 -0600

    Last changes saved

--- backend/connect7/tests/test_forms.py
+++ backend/connect7/tests/test_forms.py
@@ -19,25 +19,33 @@ class EventFormTest(TestCase):
     def test_forms(self):
         time_in_future = datetime.now() + timedelta(hours=4)
         form_data = {
-            'event_date': date.today() + timedelta(days=1),
-            'begins': datetime.now().strftime("%H:%M:%S"),
-            'ends': time_in_future.strftime("%H:%M:%S"),
-            'multi_day_event': False,
-            'multi_day_ends_date': '',
-            'repeat': 'W',
-            'location': 'Test',
-            'event_name': 'Event Test Name',
-            'description': 'Event Test Description',
+            'event_date': '11/11/2020',
+            'begins': time_in_future.strftime("%H:%M:%S"),
+            'ends': datetime.now().strftime("%H:%M:%S"),
+            'multi_day_event': True,
+            'multi_day_ends_date': '12/12/2020',
+            'repeat': 'NTESTS',
+            'location': 'Location test',
+            'event_name': 'TEst name',
+            'description': 'Event description',
         }
 
         event_form = EventForm(data=form_data)
 
+        self.assertTrue(expr=self.date_validation(date_text=form_data['event_date']),
+                        msg='End Date value for Multi Day Event is not valid.')
+
         if form_data['multi_day_event']:
             self.assertTrue(expr=self.date_validation(date_text=form_data['multi_day_ends_date']),
                             msg='End Date value for Multi Day Event is not valid.')
+
+            form_data['event_date'] = datetime.strptime(form_data['event_date'], '%d/%m/%Y')
+            form_data['multi_day_ends_date'] = datetime.strptime(form_data['multi_day_ends_date'], '%d/%m/%Y')
+
             self.assertLess(a=form_data['event_date'], b=form_data['multi_day_ends_date'],
                             msg='End date should be less than Start date')
-            self.assertEqual(first=len(form_data['repeat']), second=0,
+
+            self.assertEqual(first=form_data['repeat'], second='N',
                              msg='Repeat is not allowed when if it is a multi day event.')
         else:
             self.assertEqual(first=len(form_data['multi_day_ends_date']), second=0,
